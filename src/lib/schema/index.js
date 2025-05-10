bc705ac1616ce2b144f2327fbdc14a0f09758789
maxkleshchyonok <56303675+maxkleshchyonok@users.noreply.github.com>
2025-05-10 22:29:02 +0300

    fix: schema File error

--- src/lib/schema/index.js
+++ src/lib/schema/index.js
@@ -1,5 +1,32 @@
 import { z } from "zod";
 
+const photoValidation =
+  typeof window !== "undefined"
+    ? z
+        .instanceof(File, { message: "Photo is required and must be a file." })
+        .refine((file) => file.size <= 5 * 1024 * 1024, {
+          message: "File size must be less than 5MB.",
+        })
+        .refine((file) => ["image/jpeg", "image/jpg"].includes(file.type), {
+          message: "Only JPG or JPEG formats.",
+        })
+        .refine(
+          (file) =>
+            new Promise((resolve) => {
+              const img = new Image();
+              img.src = URL.createObjectURL(file);
+              img.onload = () => {
+                URL.revokeObjectURL(img.src);
+                resolve(img.width >= 70 && img.height >= 70);
+              };
+              img.onerror = () => resolve(false);
+            }),
+          {
+            message: "Min resolution is 70x70 pixels.",
+          }
+        )
+    : z.any(); // Skip validation on the server
+
 export const signUpSchema = z.object({
   name: z
     .string()
@@ -18,27 +45,5 @@ export const signUpSchema = z.object({
     .regex(/^[\d+]+$/gm, "Incorrect symbol")
     .max(13, "Invalid phone number"),
   position_id: z.any(),
-  photo: z
-    .instanceof(File, { message: "Photo is required and must be a file." })
-    .refine((file) => file.size <= 5 * 1024 * 1024, {
-      message: "File size must be less than 5MB.",
-    })
-    .refine((file) => ["image/jpeg", "image/jpg"].includes(file.type), {
-      message: "Only JPG or JPEG formats.",
-    })
-    .refine(
-      (file) =>
-        new Promise((resolve) => {
-          const img = new Image();
-          img.src = URL.createObjectURL(file);
-          img.onload = () => {
-            URL.revokeObjectURL(img.src);
-            resolve(img.width >= 70 && img.height >= 70);
-          };
-          img.onerror = () => resolve(false);
-        }),
-      {
-        message: "Min resolution is 70x70 pixels.",
-      }
-    ),
+  photo: photoValidation,
 });
